3fdf91419630a17e85b3afd32c8849a5bf76959e
raymond <raymondkxng17@gmail.com>
2024-07-19 18:44:41 +0200

    commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,29 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 function page() {
+    const loggedIn = {firstName: "Raymond"};
+
   return (
-    <div>Home</div>
+    <div className='home'>
+        <div className="home-content">
+            <header className="home-header">
+               <HeaderBox 
+                type="greeting"
+                title="Welcome"
+                user={loggedIn?.firstName || "Guest"}
+                subtext="Acess and manage your account and transactions effortlessley"
+               />
+
+               <TotalBalanceBox
+                accounts = {[]}
+                totalBanks={1}
+                totalCurrentBalance={24360.62}
+               />
+            </header>
+        </div>
+    </div>
   )
 }
 
